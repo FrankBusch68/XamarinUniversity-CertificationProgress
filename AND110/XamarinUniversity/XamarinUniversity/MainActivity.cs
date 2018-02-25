cfb6bbed67e5929389ec52a80eb1b741df5ca748
idenardi <ivan.denardi@gmail.com>
2018-02-25 20:25:06 -0300

    AND110 - Exercise 1

---
+++ AND110/XamarinUniversity/XamarinUniversity/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace XamarinUniversity
+{
+    [Activity(Label = "XamarinUniversity", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+
+            var instructorList = FindViewById<ListView>(Resource.Id.instructorListView);
+            instructorList.Adapter = new ArrayAdapter<Instructor>(this, Android.Resource.Layout.SimpleListItem1, InstructorData.Instructors);
+        }
+    }
+}
+
