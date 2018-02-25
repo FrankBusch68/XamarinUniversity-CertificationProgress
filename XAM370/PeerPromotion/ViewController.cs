d7155e69fe23d9514f231765764184d9e3614297
idenardi <ivan.denardi@gmail.com>
2018-02-25 19:56:32 -0300

    XAM370 - Exercise 4

---
+++ XAM370/PeerPromotion/ViewController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using UIKit;
+using System.Threading;
+
+namespace PeerPromotion
+{
+    public partial class ViewController : UIViewController
+    {
+        static int Counter;
+
+        protected ViewController (IntPtr handle) : base (handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad ()
+        {
+            Interlocked.Increment (ref Counter);
+            Debug.WriteLine ("ViewController created, {0} instances in memory.", Counter);
+
+            base.ViewDidLoad ();
+
+        }
+
+        protected override void Dispose (bool disposing)
+        {
+            base.Dispose (disposing);
+            Debug.WriteLine ("ViewController disposed, {0} instances left.", 
+                             Interlocked.Decrement(ref Counter));
+        }
+
+        partial void slider_ValueChanged(UISlider sender)
+        {
+            ValueLabel.Text = Math.Round(TheSlider.Value).ToString();
+        }
+    }
+}
+
---
+++ XAM370/PeerPromotion/ViewController.designer.cs
@@ -0,0 +1,42 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace PeerPromotion
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider TheSlider { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel ValueLabel { get; set; }
+
+        [Action ("slider_ValueChanged:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void slider_ValueChanged (UIKit.UISlider sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (TheSlider != null) {
+                TheSlider.Dispose ();
+                TheSlider = null;
+            }
+
+            if (ValueLabel != null) {
+                ValueLabel.Dispose ();
+                ValueLabel = null;
+            }
+        }
+    }
+}
