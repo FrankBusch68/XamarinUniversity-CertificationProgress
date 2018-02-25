c903e81494862cc05911716f1160b39408558248
idenardi <ivan.denardi@gmail.com>
2018-02-25 19:56:09 -0300

    XAM370 - Exercise 3

---
+++ XAM370/UserPeers/Views/CustomParentView.cs
@@ -0,0 +1,55 @@
+using UIKit;
+using System.Diagnostics;
+using System.Threading;
+using CoreGraphics;
+
+namespace UserPeers
+{
+    public class CustomParentView : UIView
+    {
+        static int Counter;
+
+        public CustomParentView()
+        {
+            Debug.WriteLine("CustomParentView created, {0} live instances", 
+                Interlocked.Increment(ref Counter));
+
+            var childView = new CustomChildView {
+                BackgroundColor = UIColor.Blue
+                //Parent = this
+            };
+
+            Add(childView);
+        }
+
+        public override void LayoutSubviews()
+        {
+            CustomChildView childView = Subviews[0] as CustomChildView;
+            if (childView != null) {
+                childView.Frame = this.Bounds.Inset(50, 50);
+            }
+
+            base.LayoutSubviews();
+        }
+
+        protected override void Dispose (bool disposing)
+        {
+            base.Dispose (disposing);
+            Debug.WriteLine("CustomParentView disposed, {0} live instances", 
+                Interlocked.Decrement(ref Counter));
+        }
+
+        // second solution to get 0 live instance
+        //public override void RemoveFromSuperview()
+        //{
+        //    base.RemoveFromSuperview();
+        //    CustomChildView childView = Subviews[0] as CustomChildView;
+
+        //    if(childView != null)
+        //    {
+        //        childView.Parent = null;
+        //    }
+        }
+    }
+    
+}
