1dba5f8d6ae49c651e2c7e83dc5ef9874c88fd60
idenardi <ivan.denardi@gmail.com>
2018-03-01 22:08:10 -0300

    IOS115 - Exercise 5

--- IOS115/WeatherApp/WeatherTVC.cs
+++ IOS115/WeatherApp/WeatherTVC.cs
@@ -1,5 +1,6 @@
 using Foundation;
 using System;
+using System.Linq;
 using System.CodeDom.Compiler;
 using UIKit;
 using System.Collections.Generic;
@@ -10,26 +11,57 @@ namespace WeatherApp
     {
         const string CELL_ID = "id";
         List<Weather> data;
+        IGrouping<char, Weather>[] grouping;
+        string[] indices; // array to show in index
 
         public WeatherTVC(IntPtr handle) : base(handle)
         {
             data = WeatherFactory.GetWeatherData();
 
-            TableView.RegisterClassForCellReuse(typeof(WeatherCell), CELL_ID);
+            grouping = (from w in data
+                        orderby w.City[0] ascending
+                        group w by w.City[0] into g
+                        select g).ToArray();
+
+            indices = (from s in data
+                       orderby s.City ascending
+                       group s by s.City[0] into g
+                       select g.Key.ToString()).ToArray();
         }
 
         public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
         {
             var cell = (WeatherTableCell)tableView.DequeueReusableCell("cell_id");
+            var weather = grouping[indexPath.Section].ElementAt(indexPath.Row);
 
-            cell.UpdateData(data[indexPath.Row]);
-
+            cell.UpdateData(weather);
             return cell;
         }
 
         public override nint RowsInSection(UITableView tableview, nint section)
         {
-            return data.Count;
+            return grouping[section].Count();
         }
+
+        public override nint NumberOfSections(UITableView tableView)
+        {
+            return grouping.Length;
+        }
+
+        public override string TitleForHeader(UITableView tableView, nint section)
+        {
+            return grouping[section].Key.ToString();
+        }
+
+        public override string TitleForFooter(UITableView tableView, nint section)
+        {
+            return "Number of Cities: " + grouping[section].Count().ToString();
+        }
+
+        public override string[] SectionIndexTitles(UITableView tableView)
+        {
+            return indices;
+        }
+
     }
 }
