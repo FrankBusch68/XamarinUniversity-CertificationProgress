795a81f15f47745bc4c5c2742c5c593494bfe253
idenardi <ivan.denardi@gmail.com>
2018-03-06 18:56:51 -0300

    XAM301 - Exercise 3

---
+++ XAM301/QuickFlicks/QuickFlicks.XForms/QuickFlicks.XForms/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using QuickFlicks.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace QuickFlicks.XForms
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+            BindingContext = new MainViewModel();
+            InitializeComponent();
+        }
+	}
+}
